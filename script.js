7f8730358732108612c3a13f08e4d4d9247a6ef5
Lady Gaga <jessiezheqi.li@gmail.com>
2021-02-20 00:16:59 -0800

    Comment envisioned steps for main structure of program in js file

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 var generateBtn = document.querySelector("#generate");
 
 // create password generator function
+function generatePassword() {
+  // inform the user of upcoming criteria
 
-// when generate button is clicked, run function
+  // let user choose a length for password
+
+
+  // let user decide if lowercase letters are to be included
+
+  //validate user's choice of yes or no by returning the storage variable
+
+  // let user decide if uppercase letters are to be included
+
+  //validate user's choice of yes or no by returning the storage variable
+
+  // let user decide if numeric values are to be included
+
+  //validate user's choice of yes or no by returning the storage variable
+
+  // let user decide if special characters are to be included
+
+  //validate user's choice of yes or no by returning the storage variable
+
+  
+  // generate the password and store in a variable named password
+
+  // alert password on page
+}
+// when button is clicked, run function
 generateBtn.addEventListener("click", generatePassword);
